ffe69673d40972db864f191c4d13d71bbf50aef5
kwangyoulsagong <sgky0511@naver.com>
2023-05-24 21:08:49 +0900

    demo

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/register.html");
 });
 
-app.get("/mypage", (req, res) => {
+app.get("/mypage:id", (req, res) => {
  
   res.sendFile(__dirname + "/mypage.html");
 });
@@ -95,16 +95,7 @@ app.post('/search', (req, res) => {
     });
   });
 });
-app.post('/purchase', (req, res) => {
-  const productName = req.body.productName; // 제품 이름은 요청의 본문(body)에 포함됩니다.
 
-  // 여기서 구매 처리 로직을 구현합니다.
-  // 제품 이름을 활용하여 구매 작업을 수행합니다.
-
-  // 응답으로 마이페이지 또는 구매 완료 페이지 등을 보내줍니다.
-  // 이 예시에서는 구매 완료 페이지로 리다이렉션합니다.
-  res.send(productName)
-})
 app.post('/login',(req,res)=>{
   const id= req.body.id
   const pw=req.body.pw
@@ -117,14 +108,223 @@ app.post('/login',(req,res)=>{
     if (error) throw error;
     if(id == data[0].id && pw==data[0].passward){
      app.route(`/customer`).get((req,res)=>{
-      res.send(data[0].name)
+      const arr=[data[0].name, data[0].id]
+      res.send(arr)
      })
+     connection.query('SELECT COUNT(*) AS count FROM mypage WHERE id = ?', [id], (err, result) => {
+      if (err) throw err;
+
+      if (result[0].count === 0) {
+          // ID does not exist in mypagetable, so perform the insert
+          connection.query('INSERT INTO mypage (id) VALUES (?)', [id], (err, result) => {
+              if (err) throw err;
+              console.log('ID inserted into mypagetable.');
+          });
+      } else {
+          console.log('ID already exists in mypagetable. Skipping insertion.');
+      }
+
+  });
+  app.post('/searchlogin', (req, res) => {
+    const search = req.body.search;
+    const searchTerm = '%' + search + '%'; // 검색어 앞뒤에 와일드카드를 추가합니다.
+  
+    const query = 'SELECT * FROM cosmetics WHERE productname LIKE ?';
+    connection.query(query, [searchTerm], (error, results) => {
+      if (error) throw error;
+  
+      const resultArray1 = [];
+      const resultArray2 = [];
+      const resultArray3 = [];
+      const resultArray4 = [];
+      const resultArray5 = [];
+  
+      for (let i = 0; i < results.length; i++) {
+        const arr1 = results[i].company;
+        resultArray1.push(arr1);
+  
+        const arr2 = results[i].type;
+        resultArray2.push(arr2);
+  
+        const arr3 = results[i].productname;
+        resultArray3.push(arr3);
+  
+        const arr4 = results[i].price;
+        resultArray4.push(arr4);
+  
+        const arr5 = results[i].saleprice;
+        resultArray5.push(arr5);
+      }
+  
+      res.render('searchlogin', {
+        resultArray1,
+        resultArray2,
+        resultArray3,
+        resultArray4,
+        resultArray5
+      });
+    });
+  });
+  
+  app.post('/purchase', (req, res) => {
+    const selectedProduct = req.body.search1; // Selected product name
+  
+    const query = 'SELECT * FROM cosmetics WHERE productname = ?';
+    connection.query(query, [selectedProduct], (error, results) => {
+      if (error) throw error;
+  
+      const productid = results[0].productid;
+      const company = results[0].company;
+      const productname = results[0].productname;
+      const price = results[0].price;
+  
+      const randomInt = Math.floor(Math.random() * 200000) + 100000;
+  
+      // Insert the product into the user's cart
+      connection.query(
+        'INSERT INTO mycart VALUES (?, ?, ?, ?, ?, ?, "5월31일 도착")',
+        [randomInt, productid, id, company, productname, price],
+        (error, data) => {
+          if (error) throw error;
+  
+          // Retrieve the updated cart data
+          connection.query('SELECT * FROM mycart WHERE id = ?', [id], (error, data) => {
+            if (error) throw error;
+  
+            const html = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <title>구매목록/올리브영</title>
+              <style>
+                /* Add your CSS styles here */
+                body {
+                  font-family: Arial, sans-serif;
+                  background-color: #f2f2f2;
+                  margin: 0;
+                  padding: 0;
+                }
+
+                .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  padding: 20px;
+                  background-color: #fff;
+                  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                }
+
+                h1 {
+                  text-align: center;
+                  color: #0096c7;
+                  margin-bottom: 20px;
+                }
+
+                .product-details {
+                  margin-top: 20px;
+                  border-top: 1px solid #ccc;
+                  padding-top: 20px;
+                }
+
+                .product-details p {
+                  margin: 0;
+                }
+
+                .cart-items {
+                  margin-top: 20px;
+                }
+
+                .cart-item {
+                  border: 1px solid #ccc;
+                  margin-bottom: 10px;
+                  padding: 10px;
+                }
+
+                .cart-item p {
+                  margin: 0;
+                }
+
+                .arrival-date {
+                  margin-top: 20px;
+                  font-style: italic;
+                  color: #888;
+                }
+
+                .success-message {
+                  margin-top: 20px;
+                  text-align: center;
+                  color: green;
+                }
+
+                .back-button {
+                  margin-top: 20px;
+                  text-align: center;
+                }
+
+                .back-button a {
+                  display: inline-block;
+                  padding: 10px 20px;
+                  background-color: #0096c7;
+                  color: #fff;
+                  text-decoration: none;
+                  border-radius: 4px;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="container">
+                <h1>구매목록 및 구매</h1>
+
+                <div class="product-details">
+                  <h2>Selected Product Details</h2>
+                  <p><strong>Product ID:</strong> ${productid}</p>
+                  <p><strong>Company:</strong> ${company}</p>
+                  <p><strong>Product Name:</strong> ${productname}</p>
+                  <p><strong>Price:</strong> ${price}</p>
+                </div>
+
+                <div class="cart-items">
+                  <h2>Cart Items</h2>
+                  ${data
+                    .map(
+                      item => `
+                        <div class="cart-item">
+                          <p><strong>Product ID:</strong> ${item.productid}</p>
+                          <p><strong>Company:</strong> ${item.company}</p>
+                          <p><strong>Product Name:</strong> ${item.productname}</p>
+                          <p><strong>Price:</strong> ${item.price}</p>
+                        </div>
+                      `
+                    )
+                    .join('')}
+                </div>
+
+                <p class="arrival-date">예상 도착 날짜: 5월 31일 도착</p>
+
+                <p class="success-message">구매 성공! 곧 리디렉션됩니다.</p>
+
+                <div class="back-button">
+                  <a href="/user:${id}">뒤로가기</a>
+                </div>
+              </div>
+            </body>
+            </html>
+          `;
+
+          res.send(html);
+          });
+        }
+      );
+    });
+  });
+  
+  
+
      res.redirect(`/user:${id}`)
      
     }
     //res.send(rows);
   })
-  connection.query('update mypage set id=?', [id], (error, data) => {})
+  
   
   app.get('/change', (req, res) => {
     connection.query('SELECT * from mypage where id=?', [id], (error, data) => {
@@ -148,7 +348,7 @@ app.post('/update', (req, res) => {
           res.send(`
               <script type="text/javascript">
                   alert("수정되었습니다.");
-                  window.location.href = "/mypage";
+                  window.location.href = "/mypage:${id}";
               </script>
           `);
       });
@@ -161,6 +361,21 @@ app.get('/mypage', (req, res) => {
         res.json(data[0]);
         
     });
+});
+app.post('/delete', (req, res) => {
+  connection.query('DELETE FROM mycart WHERE id=?', [id], (error, data) => {
+    connection.query('DELETE FROM mypage WHERE id=?', [id], (error, data) =>{
+      connection.query('DELETE FROM customer WHERE id=?', [id], (error, data) =>{})
+    })
+    if (error) throw error;
+
+    res.send(`
+      <script type="text/javascript">
+          alert("회원 정보가 삭제되었습니다.");
+          window.location.href = "/"; // 이동할 URL을 지정해주세요.
+      </script>
+    `);
+  });
 });
 
 
